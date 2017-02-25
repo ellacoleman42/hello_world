b10e69aa5e226284b323d64f4166438995fff35f
Ella Coleman <ellaweasley10@gmail.com>
2017-02-25 20:59:14 +0000

    improved working version

--- random_number_guesser.py
+++ random_number_guesser.py
@@ -1,15 +1,17 @@
 import random
 import time
 
-number = random.randint(1,10)
+number = random.randint(1,100)
 guess = 0
 count = 0
 while number != guess:
     while guess != number:
-        guess = int(input("I'm thinking of a number between one and ten. Can you guess what it is?"))
-        if guess != number:
-            print("That is wrong. Please guess again.")
+        guess = int(input("I'm thinking of a number between one and one hundred. Can you guess what it is?"))
+        if guess > number:
+            print("That is too big. Please guess again.")
             count += 1
+        if guess < number:
+            print("That is too small. Please try again.")
         if count >= 10:
             print("You have guessed 10 times. Please wait 5 seconds before guessing again.")
             time.sleep(5)
